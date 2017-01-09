44093590896875b624c05fcea2bc52f626d85b62
r7gk <hider.oliveira@vixteam.com.br>
2017-01-09 15:13:55 -0200

    Inicio tela de configuracao.

---
+++ alltomator/src/alltomator/delagate/AutomatorLaunchConfigurationDelegate.java
@@ -0,0 +1,17 @@
+package alltomator.delagate;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.debug.core.ILaunch;
+import org.eclipse.debug.core.ILaunchConfiguration;
+import org.eclipse.debug.core.model.LaunchConfigurationDelegate;
+
+public class AutomatorLaunchConfigurationDelegate extends LaunchConfigurationDelegate {
+
+	@Override
+	public void launch(ILaunchConfiguration arg0, String arg1, ILaunch arg2,
+			IProgressMonitor arg3) throws CoreException {
+		
+	}
+
+}
---
+++ alltomator/src/alltomator/tabgroup/AlltomatorLaunchConfigurationTabGroupClass.java
@@ -0,0 +1,54 @@
+package alltomator.tabgroup;
+
+import org.eclipse.debug.core.ILaunch;
+import org.eclipse.debug.core.ILaunchConfiguration;
+import org.eclipse.debug.core.ILaunchConfigurationWorkingCopy;
+import org.eclipse.debug.ui.ILaunchConfigurationDialog;
+import org.eclipse.debug.ui.ILaunchConfigurationTab;
+import org.eclipse.debug.ui.ILaunchConfigurationTabGroup;
+
+public class AlltomatorLaunchConfigurationTabGroupClass implements ILaunchConfigurationTabGroup {
+
+	@Override
+	public void createTabs(ILaunchConfigurationDialog arg0, String arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public ILaunchConfigurationTab[] getTabs() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void initializeFrom(ILaunchConfiguration arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void launched(ILaunch arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void performApply(ILaunchConfigurationWorkingCopy arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setDefaults(ILaunchConfigurationWorkingCopy arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
